43ea269fe3866f052a068d5a484582ddfcdbe821
LouisMatos <luismatos000@gmail.com>
2025-03-09 14:47:00 -0300

    inclusão de logs e x req id

--- src/main/java/br/matosit/customer_service/application/usecases/FindCustomerUseCase.java
+++ src/main/java/br/matosit/customer_service/application/usecases/FindCustomerUseCase.java
@@ -1,6 +1,8 @@
 package br.matosit.customer_service.application.usecases;
 
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import br.matosit.customer_service.application.ports.CustomerRepository;
 import br.matosit.customer_service.domain.entities.Customer;
@@ -11,18 +13,26 @@ public class FindCustomerUseCase {
 
   private final CustomerRepository customerRepository;
 
+  Logger log = LoggerFactory.getLogger(FindCustomerUseCase.class);
+
   public FindCustomerUseCase(CustomerRepository customerRepository) {
     this.customerRepository = customerRepository;
   }
 
   public Customer execute(String id) {
     // Validação de negócio
+    log.info("Buscando cliente: {}", id);
+
     Optional<Customer> existingCustomer = customerRepository.findById(id);
+    log.info("Cliente encontrado: {}", existingCustomer);
 
     if (existingCustomer.isEmpty()) {
+      log.warn("Cliente não encontrado: {}", id);
       throw new CustomerNotFoundException(id);
     }
 
+    // Retornar
+    log.info("Cliente encontrado: {}", existingCustomer.get().toString());
     return existingCustomer.get();
   }
 }
